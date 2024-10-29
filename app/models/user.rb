ed5d1a55f36e9478c91d4b0649993842a6224801
Dev-Moulin <p.moulin.95@gmail.com>
2024-10-29 15:56:13 +0100

    validation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   belongs_to :city
   has_many :gossips
+
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
 end
